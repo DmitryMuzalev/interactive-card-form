6b903195c76b421019f81ad3f37aeb92bf3caa79
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-02-05 14:51:23 +0300

    feat: Add form structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Container } from "components/Container/Container";
+import { Form } from "components/Form/Form";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form />
+    </Container>
+  );
 }
 
 export default App;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,39 @@
 import styles from "./Form.module.scss";
 
 function Form() {
-  return <div className={styles.form}>Form</div>;
+  return (
+    <form className={styles.form}>
+      <div className={styles.formItem}>
+        <label htmlFor="name">cardholder name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="e.g. Jane Appleseed"
+        />
+      </div>
+      <div className={styles.formItem}>
+        <label htmlFor="number">card number</label>
+        <input
+          type="number"
+          id="number"
+          name="number"
+          placeholder="e.g. 1234 56789123 0000"
+        />
+      </div>
+      <div className={styles.formItems}>
+        <div className={styles.formItem}>
+          <label htmlFor="month">exp. date (mm/yy)</label>
+          <input type="number" id="month" name="month" placeholder="MM" />
+          <input type="number" id="year" name="year" placeholder="YY" />
+        </div>
+        <div className={styles.formItem}>
+          <label htmlFor="cvc">cvc</label>
+          <input type="number" id="cvc" name="cvc" placeholder="e.g. 123" />
+        </div>
+      </div>
+      <button type="submit">Confirm</button>
+    </form>
+  );
 }
 export { Form };
